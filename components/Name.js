a8691071e199d8e7149917dfea8595f67cccce29
jaqwe2301 <jaqwe2301@naver.com>
2022-12-30 04:46:46 +0900

    민혁 수정

---
+++ components/Name.js
@@ -0,0 +1,9 @@
+
+export default function Name() {
+
+    return (
+        <div>
+            <p>나는 장정우</p>
+        </div>
+    )
+}
--- components/QRScanner.js
+++ components/QRScanner.js
@@ -1,19 +1,19 @@
-import QRCode from 'qrcode.react';
+// import QRCode from 'qrcode.react';
 import { useState } from 'react';
 
 export default function QRScanner() {
-    const [result, setResult] = useState('');
+    // const [result, setResult] = useState('');
 
-    const handleScan = (data) => {
-        if (data) {
-            setResult(data);
-            console.log(result);
-        }
-    };
+    // const handleScan = (data) => {
+    //     if (data) {
+    //         setResult(data);
+    //         console.log(result);
+    //     }
+    // };
 
     return (
         <div className="m-10">
-            <QRCode className="w-[200px] h-[200px]" value="QRcode-value" onScan={handleScan} />
+            {/* <QRCode className="w-[200px] h-[200px]" value="QRcode-value" onScan={handleScan} /> */}
         </div>
     )
 }
