d3b703f33b9174f08fad24af97b517bda2108542
jaqwe2301 <jaqwe2301@naver.com>
2022-12-30 05:34:17 +0900

    a

--- components/Name.js
+++
@@ -1,9 +0,0 @@
-
-export default function Name() {
-
-    return (
-        <div>
-            <p>나는 장정우</p>
-        </div>
-    )
-}
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,12 @@
 import Calendar from '../components/Calendar'
-import QRScanner from '../components/QRscanner'
-import Tree from '../components/Tree'
 import Animal from './../components/Animal';
-import Name from './../components/Name';
 
 export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center">
       <div className='home'>
-        <Name />
         <Animal />
-        <Tree />
         <Calendar />
-        <QRScanner />
       </div>
     </div>
   )
