c0d55b23836622bdcbfaaab8677ff2369326ee35
jaqwe2301 <jaqwe2301@naver.com>
2022-12-30 04:51:44 +0900

    민혁 다시 수정

--- components/Calendar.js
+++
@@ -1,9 +0,0 @@
-
-export default function Calendar() {
-    
-    return (
-        <div className="flex justify-center items-center w-[300px] h-[300px] border-1 rounded-xl shadow-md">
-            
-        </div>
-    )
-}
