d0db88af490d587777b32adb6c1393d5a8c764fd
Jaeho Yang <ho4121e@gmail.com>
2022-12-29 17:55:59 +0900

    구조 생성

---
+++ components/Calendar.js
@@ -0,0 +1,9 @@
+
+export default function Calendar() {
+    
+    return (
+        <div className="flex justify-center items-center w-[300px] h-[300px] border-1 rounded-xl shadow-md">
+            
+        </div>
+    )
+}
---
+++ components/Tree.js
@@ -0,0 +1,9 @@
+
+export default function Tree() {
+    
+    return (
+        <div className="flex justify-center items-center w-[300px] h-[300px] border-1 rounded-xl shadow-md">
+            <img src="/tree.png" />
+        </div>
+    )
+}
